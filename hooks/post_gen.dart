f121389e835f8e59f675ae39aacedc33196b4eb8
renankanu <renankanu@gmail.com>
2022-10-21 21:01:50 -0300

    feature: sort imports

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -19,6 +19,7 @@ Future<void> addRoute(HookContext context) async {
       '_Paths', "$declareRoute '/{{feature_name.paramCase()}}';");
 
   routesFile.createSync(recursive: true);
+  content = sortImports(content);
   routesFile.writeAsStringSync(content);
   await Process.run(
     "dart",
@@ -48,7 +49,8 @@ Future<void> addPage(HookContext context) async {
   lines.insert(0, "import '../modules/${name}/views/${name}_view.dart';");
 
   appPagesFile.createSync(recursive: true);
-  appPagesFile.writeAsStringSync(lines.join('\n'));
+  final sortedLines = sortImports(lines.join('\n'));
+  appPagesFile.writeAsStringSync(sortedLines);
   await Process.run(
     "dart",
     ["format", appPagesFile.path],
@@ -73,4 +75,93 @@ extension StringExt on String {
     var newValue = substring(0, index) + value + substring(index);
     return newValue;
   }
+}
+
+String sortImports(
+  String content,
+) {
+  content = _formatterDartFile(content);
+  var lines = LineSplitter.split(content).toList();
+
+  var contentLines = <String>[];
+
+  var library = <String>[];
+  var dartImports = <String>[];
+  var flutterImports = <String>[];
+  var packageImports = <String>[];
+  var projectRelativeImports = <String>[];
+  var projectImports = <String>[];
+  var exports = <String>[];
+
+  var stringLine = false;
+  for (var i = 0; i < lines.length; i++) {
+    if (lines[i].startsWith('import ') &&
+        !stringLine &&
+        lines[i].endsWith(';')) {
+      if (lines[i].contains('dart:')) {
+        dartImports.add(lines[i]);
+      } else if (lines[i].contains('package:flutter/')) {
+        flutterImports.add(lines[i]);
+      } else if (!lines[i].contains('package:')) {
+        projectRelativeImports.add(lines[i]);
+      } else if (lines[i].contains('package:')) {
+        if (!lines[i].contains('package:flutter/')) {
+          packageImports.add(lines[i]);
+        }
+      }
+    } else if (lines[i].startsWith('export ') &&
+        lines[i].endsWith(';') &&
+        !stringLine) {
+      exports.add(lines[i]);
+    } else if (lines[i].startsWith('library ') &&
+        lines[i].endsWith(';') &&
+        !stringLine) {
+      library.add(lines[i]);
+    } else {
+      var containsThreeQuotes = lines[i].contains("'''");
+      if (containsThreeQuotes) {
+        stringLine = !stringLine;
+      }
+      contentLines.add(lines[i]);
+    }
+  }
+
+  if (dartImports.isEmpty &&
+      flutterImports.isEmpty &&
+      packageImports.isEmpty &&
+      projectImports.isEmpty &&
+      projectRelativeImports.isEmpty &&
+      exports.isEmpty) {
+    return content;
+  }
+
+  dartImports.sort();
+  flutterImports.sort();
+  packageImports.sort();
+  projectImports.sort();
+  projectRelativeImports.sort();
+  exports.sort();
+  library.sort();
+
+  var sortedLines = <String>[];
+
+  sortedLines.addAll([
+    ...library,
+    '',
+    ...dartImports,
+    '',
+    ...flutterImports,
+    '',
+    ...packageImports,
+    '',
+    ...projectImports,
+    '',
+    ...projectRelativeImports,
+    '',
+    ...exports,
+    '',
+    ...contentLines
+  ]);
+
+  return _formatterDartFile(sortedLines.join('\n'));
 }
